af210447e00a8c2775955c80fb4432aac63921d9
sebasbit <sebastian.torres0426@gmail.com>
2022-05-05 16:54:21 -0500

    Refactor test when a correct phone is provided

--- test/cophone.test.js
+++ test/cophone.test.js
@@ -20,15 +20,8 @@ test('it should return null when the phone is empty', () => {
 })
 
 test('it should return a formatted cell phone when a good one is provided', () => {
-    const phone = '300000000'
-    const formated = cophone.format(phone)
-    expect(formated).toBe('300 000 000')
-})
-
-test('it should return a formatted cell phone when a good one but different is provided', () => {
-    const phone = '300000001'
-    const formated = cophone.format(phone)
-    expect(formated).toBe('300 000 001')
+    expect(cophone.format('300000000')).toBe('300 000 000')
+    expect(cophone.format('300000001')).toBe('300 000 001')
 })
 
 test('it should return null when the cell phones length is invalid', () => {
